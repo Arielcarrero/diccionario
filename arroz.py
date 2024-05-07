db9e74428ffed6e6cbb6120eb2095d3ce1ae4630
Arielcarrero <167259183+Arielcarrero@users.noreply.github.com>
2024-05-07 20:24:53 -0300

    Update arroz.py

--- arroz.py
+++ arroz.py
@@ -1,18 +1,45 @@
-meme_dict = {
-            "CRINGE": "Algo excepcionalmente raro o embarazoso",
-            "LOL": "Una respuesta común a algo gracioso",
-            "XD":"una carita muriendo de risa",
-            "WTF":"es una mala palabra en ingles",
-            "HDP":"es una mala palabra en ingles",
-            "VR":"significa realidad virtual"
-}
-            
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
-if word in meme_dict.keys():
-    
-        
-    # ¿Qué debemos hacer si se encuentra la palabra?
-    print(meme_dict[word])
-else:
-    print("no e encontrado esa palabra😓")
-    # ¿Qué hacer si no se encuentra la palabra?  
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def saludos(ctx):
+    await ctx.send("Bienvenidos")
+
+@bot.command()
+async def suma(ctx, num1:int, num2:int ):
+    resultado=num1+num2
+    await ctx.send(resultado)
+
+@bot.command()
+async def multiplica(ctx, num1:int, num2:int ):
+    resultado=num1*num2
+    await ctx.send(resultado)
+
+@bot.command()
+async def resta(ctx, num1:int, num2:int ):
+    resultado=num1-num2
+    await ctx.send(resultado)
+
+@bot.command()
+async def divide(ctx, num1:int, num2:int ):
+    resultado=num1/num2
+    await ctx.send(resultado)
+bot.run()
